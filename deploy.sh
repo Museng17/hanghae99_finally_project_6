f6208fa5efec3ff587d50857321ac31cb451382b
MinjuKim <kpoong02@naver.com>
2022-06-29 01:58:15 +0900

    왜 안 될까

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,21 @@
-#!/usr/bin/env bash
-
-REPOSITORY=/opt/testapp
+REPOSITORY=/home/ec2-user/app
 cd $REPOSITORY
 
-APP_NAME=action_codedeploy
+APP_NAME=FinalProject #1
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
-if [ -z $CURRENT_PID ]
+if [ -z $CURRENT_PID ] #2
 then
-  echo "> 종료할것 없음."
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -9 $CURRENT_PID"
-  kill -15 $CURRENT_PID
+  echo "> kill -15 $CURRENT_PID"
+  sudo kill -15 $CURRENT_PID
   sleep 5
 fi
 
-echo "> $JAR_PATH 배포"
-nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+echo "> $JAR_PATH 배포" #3
+nohup java -jar /home/ec2-user/app/build/libs/FinalProject-0.0.1-SNAPSHOT.jar
+--spring.config.location=/home/ec2-user/application.properties > /dev/null 2> /dev/null < /dev/null &
