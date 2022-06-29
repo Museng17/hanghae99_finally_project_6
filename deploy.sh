ec8865782b5be2baa4556777f1a3e1380ab23280
MinjuKim <kpoong02@naver.com>
2022-06-29 12:25:57 +0900

    github actions test final_final_final

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 REPOSITORY=/home/ubuntu/app
 cd $REPOSITORY
 
-APP_NAME=FinalProject #1
+APP_NAME=finalproject #1
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
