bc05772be582de2b679e5d77396adf3fad820d76
MinjuKim <kpoong02@naver.com>
2022-06-29 13:46:30 +0900

    github action 주석 정리

--- src/main/java/com/hanghae99/finalproject/controller/TestController.java
+++ src/main/java/com/hanghae99/finalproject/controller/TestController.java
@@ -3,6 +3,7 @@ package com.hanghae99.finalproject.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// github actions test용 controller
 @RestController
 public class TestController {
     @GetMapping("/test")
