4d76cf32bd824885185e91dc397f1167811ed140
MinjuKim <kpoong02@naver.com>
2022-06-29 13:17:40 +0900

    github actions testcontroller add

---
+++ src/main/java/com/hanghae99/finalproject/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.hanghae99.finalproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        String testStr = "Hello World";
+        System.out.println(testStr);
+        return testStr;
+    }
+}
