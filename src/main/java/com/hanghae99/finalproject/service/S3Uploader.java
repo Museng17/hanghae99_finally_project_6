a76500e9503bbc6dfa541da753ea7629e20b696f
MinjuKim <kpoong02@naver.com>
2022-07-07 19:27:27 +0900

    S3 Dto 생성 및 코드 리팩토링

--- src/main/java/com/hanghae99/finalproject/service/S3Uploader.java
+++ src/main/java/com/hanghae99/finalproject/service/S3Uploader.java
@@ -1,19 +1,24 @@
 package com.hanghae99.finalproject.service;
 
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-import org.springframework.web.multipart.MultipartFile;
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Optional;
 import java.util.UUID;
+
+import com.hanghae99.finalproject.model.dto.responseDto.FileUploadResponse;
+import com.hanghae99.finalproject.model.entity.Users;
+import com.hanghae99.finalproject.model.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -21,35 +26,44 @@ import java.util.UUID;
 public class S3Uploader {
 
     private final AmazonS3Client amazonS3Client;
+    private final UserRepository userRepository;
 
     @Value("${cloud.aws.s3.bucket}")
-    public String bucket;
+    public String bucket; // S3 버킷 이름
 
-    public String upload(MultipartFile multipartFile, String dirName) throws IOException {
-        File uploadfile = convert(multipartFile)
-                .orElseThrow(() -> new RuntimeException("error: MultipartFile -> file convert fail"));
+    public FileUploadResponse upload(Long userId, MultipartFile multipartFile, String dirName) throws IOException {
 
-        return upload(uploadfile, dirName);
+        File uploadFile = convert(multipartFile)
+                .orElseThrow(() -> new RuntimeException("MultipartFile -> file convert fail"));
+
+        return upload(userId, (MultipartFile) uploadFile, dirName);
     }
 
-    private String upload(File uploadFile, String dirName) {
-        String fileName = dirName + "/" + UUID.randomUUID() + uploadFile.getName();
+    private FileUploadResponse upload(Long userId, File uploadFile, String dirName) {
+        String fileName = dirName + "/" + uploadFile.getName();
         String uploadImageUrl = putS3(uploadFile, fileName);
         removeNewFile(uploadFile);
-        return uploadImageUrl;
+
+        Users user = userRepository.findById(userId).get();
+        user.setImgPath(uploadImageUrl);
+
+        return new FileUploadResponse(fileName, uploadImageUrl);
     }
 
+    // S3로 업로드
     private String putS3(File uploadFile, String fileName) {
-        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile).withCannedAcl(CannedAccessControlList.PublicRead));
+        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile)
+                .withCannedAcl(CannedAccessControlList.PublicRead));
         return amazonS3Client.getUrl(bucket, fileName).toString();
     }
 
+    // 로컬에 저장된 이미지 지우기
     private void removeNewFile(File targetFile) {
         if (targetFile.delete()) {
-            log.info("File delete success");
+            log.info("파일이 삭제되었습니다.");
             return;
         }
-        log.info("File delete fail");
+        log.info("파일 삭제 실패");
     }
 
     // 로컬에 파일 업로드 하기
