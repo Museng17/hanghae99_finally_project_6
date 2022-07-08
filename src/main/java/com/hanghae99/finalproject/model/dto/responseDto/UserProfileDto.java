4e9decf0ec6d80ec681544e8d6e98cf6f57f5ec8
MinjuKim <kpoong02@naver.com>
2022-07-08 17:54:33 +0900

    UserProfileDto 생성

---
+++ src/main/java/com/hanghae99/finalproject/model/dto/responseDto/UserProfileDto.java
@@ -0,0 +1,18 @@
+package com.hanghae99.finalproject.model.dto.responseDto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserProfileDto {
+    private Long id;
+    private String nickname;
+    private String imgPath;
+    private String information;
+    private long followingCnt;
+    private long followerCnt;
+    private long boardCnt;
+    private long folderCnt;
+}
