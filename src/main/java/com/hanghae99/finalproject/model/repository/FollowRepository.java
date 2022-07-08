6adb7e860f5c1ec806dec3c2a52f9d29fca846a2
MinjuKim <kpoong02@naver.com>
2022-07-08 17:56:28 +0900

    follow Count 쿼리문 작성

--- src/main/java/com/hanghae99/finalproject/model/repository/FollowRepository.java
+++ src/main/java/com/hanghae99/finalproject/model/repository/FollowRepository.java
@@ -3,9 +3,16 @@ package com.hanghae99.finalproject.model.repository;
 import com.hanghae99.finalproject.model.entity.Follow;
 import com.hanghae99.finalproject.model.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface FollowRepository  extends JpaRepository<Follow, Long> {
 
-    Follow findFollowByFollowingAndFollower(Users following_id, Users follower_id);
+    Follow findByFollowingIdAndFollowerId(Long following_id, Long follower_id);
+
+    @Query(value = "SELECT COUNT(*) FROM follow WHERE follower_Id = ?1", nativeQuery = true)
+    long findFollowerCountById(long Id);
+
+    @Query(value = "SELECT COUNT(*) FROM follow WHERE following_Id = ?1", nativeQuery = true)
+    long findFollowingCountById(long id);
 
 }
