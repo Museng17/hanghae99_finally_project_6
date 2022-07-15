2118e72e7bbfd69723a2252f5935f0c3d69be675
백현명 <coffee_soul@navercom>
2022-07-15 20:34:31 +0900

    안쓰는 코드 제거 및 인덴스 정렬

--- src/main/java/com/hanghae99/finalproject/model/repository/BoardRepository.java
+++ src/main/java/com/hanghae99/finalproject/model/repository/BoardRepository.java
@@ -19,11 +19,6 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
 
     Optional<Board> findByIdAndUsersIdNot(Long boardId, Long id);
 
-    @Query("select count(id) from Board where users.id = ?1")
-    Long findBoardCount(Long id);
-
-    List<Board> findByUsers(Users user);
-
     @Query("select b from Board b where  b.folder.id = ?1 and b.title LIKE case when ?2 = '%all%' then '%%' else ?2 end and b.category in ?3 and b.users.id = ?4 and b.status IN ?5")
     Page<Board> findByFolderIdAndTitleContainingAndCategoryIn(Long folderId,
                                                               String keyword,
@@ -41,6 +36,7 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
 
     @Query("select b from Board b where  b.status = ?1 and b.title LIKE case when ?2 = '%all%' then '%%' else ?2 end and b.category in ?3")
     Page<Board> findAllByStatusAndTitleContainingAndCategoryIn(DisclosureStatus status, String keyword, List<CategoryType> categoryTypeList, Pageable pageable);
+
     @Query("select b from Board b where  b.status = ?1 and b.title LIKE case when ?2 = '%all%' then '%%' else ?2 end and b.category in ?3")
     List<Board> findAllByStatusAndTitleContainingAndCategoryIn(DisclosureStatus status, String keyword, List<CategoryType> categoryTypeList);
 
