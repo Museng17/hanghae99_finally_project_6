b4ef153e554785fe98a1d72ce2034fca98ef8fd3
백현명 <coffee_soul@navercom>
2022-07-20 20:16:01 +0900

    스케쥴러 및 인증시간도 추가

--- src/main/java/com/hanghae99/finalproject/service/MailService.java
+++ src/main/java/com/hanghae99/finalproject/service/MailService.java
@@ -6,8 +6,11 @@ import com.hanghae99.finalproject.singleton.CertificationMap;
 import com.hanghae99.finalproject.util.mail.MailUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
+
+import java.util.ListIterator;
 
 @Slf4j
 @Service
@@ -26,11 +29,9 @@ public class MailService {
             context.setVariable("massage", massage);
             mailUtils.sendEmail(mailUtils.makeMassageHtml("이메일 인증", mailRequestDto.getEmail(), "mail", context));
         } catch (Exception e) {
-            log.info(e.getMessage());
+            log.info("이메일 전송 실패");
             return new MassageResponseDto(501, "전송실패 : " + e.getMessage());
         }
-
-        log.info("이메일 전송 완료");
         certificationMap.put(mailRequestDto.getEmail(), massage);
         return new MassageResponseDto(200, "전송완료");
     }
@@ -42,5 +43,29 @@ public class MailService {
         }
         certificationMap.remove(mailRequestDto.getEmail());
         return new MassageResponseDto(200, "일치");
+    }
+
+    @Scheduled(cron = "00 08 20 * * *")
+    private void singletonRemove2() {
+        log.info("스케쥴러 작동");
+        log.info("삭제되기전 인증요청 카운트 : " + certificationMap.getCertificationMapSize());
+        int checkCnt = 0;
+        try {
+            ListIterator<String> listIterator = certificationMap.getUsedList().listIterator();
+            for (ListIterator<String> iterator = listIterator; iterator.hasNext(); ) {
+                String key = iterator.next();
+                if (certificationMap.isTimeOver(key)) {
+                    certificationMap.remove(key);
+                    iterator.remove();
+                    checkCnt++;
+                }
+            }
+            log.info("스케쥴러 작동 완료 삭제된 데이터 카운트 : " + checkCnt);
+            throw new RuntimeException();
+        } catch (Exception e) {
+            log.info("스케쥴러 작동 실패 에러 메세지 : ");
+            log.info(e.toString());
+        }
+
     }
 }
