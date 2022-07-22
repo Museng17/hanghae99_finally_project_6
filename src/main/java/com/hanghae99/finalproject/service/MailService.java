04e1ca109e6bf3899189975144207fbdaee01863
백현명 <coffee_soul@navercom>
2022-07-22 11:43:23 +0900

    서버 재가동

--- src/main/java/com/hanghae99/finalproject/service/MailService.java
+++ src/main/java/com/hanghae99/finalproject/service/MailService.java
@@ -40,27 +40,27 @@ public class MailService {
         return new MessageResponseDto(200, "일치");
     }
 
-    @Scheduled(cron = "00 00 10 * * *")
-    private void singletonRemove() {
-        log.info("스케쥴러 작동");
-        log.info("삭제되기전 인증요청 카운트 : " + certificationMap.getCertificationMapSize());
-        int checkCnt = 0;
-        try {
-            ListIterator<String> listIterator = certificationMap.getUsedList().listIterator();
-            for (ListIterator<String> iterator = listIterator; iterator.hasNext(); ) {
-                String key = iterator.next();
-                if (certificationMap.isTimeOver(key)) {
-                    certificationMap.remove(key);
-                    iterator.remove();
-                    checkCnt++;
-                }
-            }
-            log.info("스케쥴러 작동 완료 삭제된 데이터 카운트 : " + checkCnt);
-        } catch (Exception e) {
-            log.info("스케쥴러 작동 실패 에러 메세지 : ");
-            log.info(e.toString());
-        }
-    }
+//    @Scheduled(cron = "00 00 10 * * *")
+//    private void singletonRemove() {
+//        log.info("스케쥴러 작동");
+//        log.info("삭제되기전 인증요청 카운트 : " + certificationMap.getCertificationMapSize());
+//        int checkCnt = 0;
+//        try {
+//            ListIterator<String> listIterator = certificationMap.getUsedList().listIterator();
+//            for (ListIterator<String> iterator = listIterator; iterator.hasNext(); ) {
+//                String key = iterator.next();
+//                if (certificationMap.isTimeOver(key)) {
+//                    certificationMap.remove(key);
+//                    iterator.remove();
+//                    checkCnt++;
+//                }
+//            }
+//            log.info("스케쥴러 작동 완료 삭제된 데이터 카운트 : " + checkCnt);
+//        } catch (Exception e) {
+//            log.info("스케쥴러 작동 실패 에러 메세지 : ");
+//            log.info(e.toString());
+//        }
+//    }
 
     public MessageResponseDto sendEmailAndUpdatePassword(MailRequestDto mailRequestDto) {
         String str = getTempPassword();
