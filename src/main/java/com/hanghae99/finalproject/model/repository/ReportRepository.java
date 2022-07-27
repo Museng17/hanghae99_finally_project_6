1e40ef44a306f0da9edd3424e7e689c066aad909
백현명 <coffee_soul@navercom>
2022-07-27 14:05:54 +0900

    폴더 삭제시 신고목록 삭제후 삭제로 변경

--- src/main/java/com/hanghae99/finalproject/model/repository/ReportRepository.java
+++ src/main/java/com/hanghae99/finalproject/model/repository/ReportRepository.java
@@ -3,5 +3,9 @@ package com.hanghae99.finalproject.model.repository;
 import com.hanghae99.finalproject.model.entity.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReportRepository extends JpaRepository<Report, Long> {
+
+    void deleteAllByBadfolderIdIn(List<Long> dbLongList);
 }
