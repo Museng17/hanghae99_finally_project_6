02eddec46ae6feb0de222a69a7f07530957377ae
백현명 <coffee_soul@navercom>
2022-07-28 11:03:38 +0900

    재기동

--- src/main/java/com/hanghae99/finalproject/util/mail/MailUtils.java
+++ src/main/java/com/hanghae99/finalproject/util/mail/MailUtils.java
@@ -105,7 +105,6 @@ public class MailUtils {
         prop.put("mail.smtp.debug", "true");
         prop.put("mail.smtp.starttls.enable", "true");
         prop.put("mail.smtp.EnableSSL.enable", "true");
-
         javaMailSender.setJavaMailProperties(prop);
         return javaMailSender;
     }
